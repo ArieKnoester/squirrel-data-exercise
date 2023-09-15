b111d806a2fc27984f079e28276899d709466dba
Arie Knoester <arikstr@gmail.com>
2023-09-15 12:18:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# squirrel-data-exercise
+
+A small exercise from a Python course as an introduction to the
+Pandas module. The goal was to get row counts of the 3 Primary Fur
+Colors, create a new DataFrame, and write the data to a new .csv.
---
+++ main.py
@@ -0,0 +1,25 @@
+# Data is from NYC Open Data.
+# https://data.cityofnewyork.us/Environment/2018-Central-Park-Squirrel-Census-Squirrel-Data/vfnx-vebw
+import pandas
+
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+# My original approach. While this technically worked, my csv did not look
+# the same as the instructors.
+# squirrel_count = data.groupby(["Primary Fur Color"]).size()
+# squirrel_count_df = pandas.DataFrame(squirrel_count).reset_index()
+# squirrel_count_df.to_csv("squirrel_count_via_groupby.csv", header=False)
+
+# This approach matched the instructors csv exactly.
+squirrel_color_count = {
+    "Fur Color": ["grey", "red", "black"],
+    "Count": [
+        sum(data["Primary Fur Color"] == "Gray"),
+        sum(data["Primary Fur Color"] == "Cinnamon"),
+        sum(data["Primary Fur Color"] == "Black")
+    ]
+}
+
+squirrel_data = pandas.DataFrame(squirrel_color_count)
+squirrel_data.to_csv("squirrel_data.csv")
